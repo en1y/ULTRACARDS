9a9d013be3bd81414f2a61a33f3b0ee3547907d4
en1y <marsentiy.cooo@gmail.com>
2025-07-13 02:21:47 +0200

    created PlayerInterface

--- game-logic/templates/game-template/src/main/java/com/ultracards/templates/game/interfaces/PlayerInterface.java
+++ game-logic/templates/game-template/src/main/java/com/ultracards/templates/game/interfaces/PlayerInterface.java
@@ -3,11 +3,33 @@ package com.ultracards.templates.game.interfaces;
 import com.ultracards.templates.cards.AbstractCard;
 import com.ultracards.templates.cards.CardTypeInterface;
 import com.ultracards.templates.cards.CardValueInterface;
+import com.ultracards.templates.game.model.AbstractDeck;
 import com.ultracards.templates.game.model.AbstractHand;
+
+import java.util.Objects;
 
 public interface PlayerInterface
         <CardType extends CardTypeInterface,
                 CardValue extends CardValueInterface,
                 Card extends AbstractCard<CardType, CardValue>,
-                Hand extends AbstractHand<CardType, CardValue, Card>>{
+                Hand extends AbstractHand<CardType, CardValue, Card>,
+                Deck extends AbstractDeck<CardType, CardValue, Card, Hand>> {
+
+    default void getHandFromDeck(Deck deck, int cardsInHandNumber) {
+        Objects.requireNonNull(deck, "deck");
+        setHand(
+                deck.createHand(cardsInHandNumber)
+        );
+    }
+
+    default void playCard(Card card) {
+        Objects.requireNonNull(card);
+        getHand().drawCard(card);
+    }
+
+    String getName();
+    Hand getHand();
+
+    void setHand(Hand hand);
+
 }
