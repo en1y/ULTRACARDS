3e709047d4c3a67c1c6801e84e3ae4ec7d620c54
en1y <marsentiy.cooo@gmail.com>
2025-07-13 15:28:54 +0200

    implemented the AbstractDeck

--- game-logic/templates/game-template/src/main/java/com/ultracards/templates/game/model/AbstractDeck.java
+++ game-logic/templates/game-template/src/main/java/com/ultracards/templates/game/model/AbstractDeck.java
@@ -5,6 +5,8 @@ import com.ultracards.templates.cards.CardTypeInterface;
 import com.ultracards.templates.cards.CardValueInterface;
 import com.ultracards.templates.game.interfaces.DeckInterface;
 
+import java.util.List;
+
 public abstract class AbstractDeck
         <CardType extends CardTypeInterface,
                 CardValue extends CardValueInterface,
@@ -12,4 +14,31 @@ public abstract class AbstractDeck
                 Hand extends AbstractHand<CardType, CardValue, Card>>
         implements DeckInterface
             <CardType, CardValue, Card, Hand, AbstractDeck<CardType, CardValue, Card, Hand>> {
+
+    private List<Card> cards;
+    private int size;
+
+    public AbstractDeck(int size) {
+        init(size);
+    }
+
+    @Override
+    public int getSize() {
+        return size;
+    }
+
+    @Override
+    public List<Card> getCards() {
+        return cards;
+    }
+
+    @Override
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    @Override
+    public void setCards(List<Card> cards) {
+        this.cards = cards;
+    }
 }
