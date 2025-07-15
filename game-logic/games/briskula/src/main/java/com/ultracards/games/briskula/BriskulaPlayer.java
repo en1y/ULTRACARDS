7a72a7b13acdbe7864ec117aa595d0ccaba6b41f
en1y <marsentiy.cooo@gmail.com>
2025-07-15 16:45:21 +0200

    added point to the player

--- game-logic/games/briskula/src/main/java/com/ultracards/games/briskula/BriskulaPlayer.java
+++ game-logic/games/briskula/src/main/java/com/ultracards/games/briskula/BriskulaPlayer.java
@@ -5,7 +5,17 @@ import com.ultracards.cards.ItalianCardValue;
 import com.ultracards.templates.game.model.AbstractPlayer;
 
 public class BriskulaPlayer extends AbstractPlayer<ItalianCardType, ItalianCardValue, BriskulaCard, BriskulaHand, BriskulaDeck> {
+    private int points;
+
     public BriskulaPlayer(String name) {
         super(name);
+    }
+
+    public void addPoints(int points) {
+        this.points += points;
+    }
+
+    public int getPoints() {
+        return points;
     }
 }
