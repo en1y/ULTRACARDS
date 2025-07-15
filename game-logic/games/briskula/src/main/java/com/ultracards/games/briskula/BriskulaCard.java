aa1d1dd856bb346b69a6bd442f5dded9fbf3269d
en1y <marsentiy.cooo@gmail.com>
2025-07-15 15:32:12 +0200

    finished implementing BriskulaCard class

--- game-logic/games/briskula/src/main/java/com/ultracards/games/briskula/BriskulaCard.java
+++ game-logic/games/briskula/src/main/java/com/ultracards/games/briskula/BriskulaCard.java
@@ -5,7 +5,7 @@ import com.ultracards.cards.ItalianCardType;
 import com.ultracards.cards.ItalianCardValue;
 
 
-public class BriskulaCard extends ItalianCard {
+public class BriskulaCard extends ItalianCard<BriskulaCard> {
 
     private final int points;
 
@@ -17,5 +17,33 @@ public class BriskulaCard extends ItalianCard {
 
     public int getPoints() {
         return points;
+    }
+
+    public int compareTo(ItalianCardType gameTrumpCardType, ItalianCardType roundTrumpCardType, BriskulaCard card) {
+        var isThisGameTrump = getType().equals(gameTrumpCardType);
+        var isOtherGameTrump = getType().equals(gameTrumpCardType);
+
+        if (isThisGameTrump && !isOtherGameTrump) {
+            return 1; // this card is trump, other is not
+        } else if (!isThisGameTrump && isOtherGameTrump) {
+            return -1; // other card is trump, this is not
+        }
+
+        var isThisRoundTrump = getType().equals(roundTrumpCardType);
+        var isOtherRoundTrump = getType().equals(roundTrumpCardType);
+
+        if (isThisRoundTrump && !isOtherRoundTrump) {
+            return 1; // this card is round trump, other is not
+        } else if (!isThisRoundTrump && isOtherRoundTrump) {
+            return -1; // other card is round trump, this is not
+        }
+
+        return this.compareTo(card);
+    }
+
+    @Override
+    public int compareTo(BriskulaCard card) {
+        var comparedPoints = Integer.compare(getPoints(), card.getPoints());
+        return comparedPoints == 0 ? super.compareTo(card): comparedPoints;
     }
 }
