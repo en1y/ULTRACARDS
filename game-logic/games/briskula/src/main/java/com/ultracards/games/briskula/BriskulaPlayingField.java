89dbb7e85987abd476dac046a3b715e8da86ad91
en1y <marsentiy.cooo@gmail.com>
2025-07-15 15:53:34 +0200

    implemented the BriskulaPlayingField

--- game-logic/games/briskula/src/main/java/com/ultracards/games/briskula/BriskulaPlayingField.java
+++ game-logic/games/briskula/src/main/java/com/ultracards/games/briskula/BriskulaPlayingField.java
@@ -6,8 +6,21 @@ import com.ultracards.templates.game.model.AbstractPlayingField;
 
 public class BriskulaPlayingField extends AbstractPlayingField<ItalianCardType, ItalianCardValue, BriskulaCard, BriskulaHand, BriskulaDeck, BriskulaPlayer> {
 
+    private final ItalianCardType gameTrumpCardType;
+
+    public BriskulaPlayingField(ItalianCardType gameTrumpCardType) {
+        super();
+        this.gameTrumpCardType = gameTrumpCardType;
+    }
+
     @Override
     public BriskulaPlayer determineRoundWinner() {
-        throw new UnsupportedOperationException("BriskulaPlayingField.determineRoundWinner() not supported yet."); // TODO: implement this method
+        var playedCards = getPlayedCards();
+        var winningCard = playedCards.stream().max(
+                (bc1, bc2) -> bc1.compareTo(gameTrumpCardType, playedCards.get(0).getType(), bc2)
+        ).orElseThrow(
+            () -> new IllegalStateException("Could not determine round winner, there were probably no cards played")
+        );
+        return getPlayerByPlayedCard(winningCard);
     }
 }
