c1bacb531d49af3b67b423219cc08470387a0cdc
en1y <marsentiy.cooo@gmail.com>
2025-07-18 16:21:46 +0200

    created UserRepository and VerificationCodeRepository

---
+++ server/src/main/java/com/ultracards/server/repositories/VerificationCodeRepository.java
@@ -0,0 +1,13 @@
+package com.ultracards.server.repositories;
+
+import com.ultracards.server.entity.UserEntity;
+import com.ultracards.server.entity.auth.VerificationCode;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+public interface VerificationCodeRepository extends JpaRepository<VerificationCode, Long> {
+    Optional<VerificationCode> findByUserAndCodeAndUsedFalse(UserEntity user, String code);
+//    void deleteByExpiredAfter(LocalDateTime cutoff); // TODO implement removal when expired
+}
