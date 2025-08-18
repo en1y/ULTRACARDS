528bcb7800082b245be5bc26a4fabb8d9c600560
en1y <marsentiy.cooo@gmail.com>
2025-08-18 08:55:19 +0200

    created EmailDTO

---
+++ game-gateway/src/main/java/com/ultracards/gateway/dto/EmailDTO.java
@@ -0,0 +1,24 @@
+package com.ultracards.gateway.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public class EmailDTO {
+    @Email
+    @NotBlank
+    private String email;
+
+    public EmailDTO() {}
+
+    public EmailDTO(String email) {
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
