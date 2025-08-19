e0d71c5789814831700cd0f0f6ef5006d2f0e132
en1y <marsentiy.cooo@gmail.com>
2025-08-19 11:57:30 +0200

    created VerificationCodeRepository.findByUserAndUsedFalse

--- server/src/main/java/com/ultracards/server/repositories/VerificationCodeRepository.java
+++ server/src/main/java/com/ultracards/server/repositories/VerificationCodeRepository.java
@@ -8,6 +8,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface VerificationCodeRepository extends JpaRepository<VerificationCode, Long> {
+    Optional<VerificationCode> findByUserAndUsedFalse(UserEntity user);
     Optional<VerificationCode> findByUserAndCodeAndUsedFalse(UserEntity user, String code);
     Optional<List<VerificationCode>> findAllByUserAndUsedFalse(UserEntity user);
 //    void deleteByExpiredAfter(LocalDateTime cutoff); // TODO implement removal when expired
