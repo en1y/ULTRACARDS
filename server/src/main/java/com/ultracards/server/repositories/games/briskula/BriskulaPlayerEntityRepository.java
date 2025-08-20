a5902de87bb3cf2ddbd0ea0d979a5d7ddec5249d
en1y <marsentiy.cooo@gmail.com>
2025-08-20 11:57:29 +0200

    changed the BriskulaPlayerEntityRepository.findByUser method

--- server/src/main/java/com/ultracards/server/repositories/games/briskula/BriskulaPlayerEntityRepository.java
+++ server/src/main/java/com/ultracards/server/repositories/games/briskula/BriskulaPlayerEntityRepository.java
@@ -8,5 +8,5 @@ import java.util.List;
 import java.util.Optional;
 
 public interface BriskulaPlayerEntityRepository extends JpaRepository<BriskulaPlayerEntity, Long> {
-    Optional<List<BriskulaPlayerEntity>> findByUser(UserEntity user);
+    List<BriskulaPlayerEntity> findByUser(UserEntity user);
 }
