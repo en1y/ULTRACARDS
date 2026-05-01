ce9249429f07d564151fa007f72ae0ee95b94481
en1y <44525782+en1y@users.noreply.github.com>
2026-05-01 21:28:27 +0200

    Add EntityGraph annotation to optimize UserGamesStats retrieval

--- server/src/main/java/com/ultracards/server/repositories/games/UserGamesStatsRepository.java
+++ server/src/main/java/com/ultracards/server/repositories/games/UserGamesStatsRepository.java
@@ -2,11 +2,13 @@ package com.ultracards.server.repositories.games;
 
 import com.ultracards.server.entity.UserEntity;
 import com.ultracards.server.entity.games.UserGamesStats;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 import java.util.UUID;
 
 public interface UserGamesStatsRepository extends JpaRepository<UserGamesStats, UUID> {
+    @EntityGraph(attributePaths = "gameStats")
     Optional<UserGamesStats> findByUser(UserEntity user);
 }
