0a3ee7ab3547df453cbc8e61d57a7cfef1c1f411
en1y <44525782+en1y@users.noreply.github.com>
2026-06-21 14:35:05 +0200

    Add support for rotated token reuse and update token repository query so user is not logged out after sending multiple requests with token having rotated

--- server/src/main/java/com/ultracards/server/repositories/auth/TokenRepository.java
+++ server/src/main/java/com/ultracards/server/repositories/auth/TokenRepository.java
@@ -3,11 +3,9 @@ package com.ultracards.server.repositories.auth;
 import com.ultracards.server.entity.UserEntity;
 import com.ultracards.server.entity.auth.TokenEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import jakarta.persistence.LockModeType;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -15,12 +13,11 @@ public interface TokenRepository extends JpaRepository<TokenEntity, UUID> {
     Optional<TokenEntity> findByToken(String token);
     Optional<TokenEntity> findFirstByUserOrderByExpiresAtDesc(UserEntity user);
 
-    @Lock(LockModeType.PESSIMISTIC_WRITE)
-    @Query("""
-            select t
-            from TokenEntity t
-            left join fetch t.replacementToken
-            where t.token = :token
-            """)
+    @Query(value = """
+            SELECT *
+            FROM tokens
+            WHERE token = :token
+            FOR UPDATE
+            """, nativeQuery = true)
     Optional<TokenEntity> findByTokenForUpdate(@Param("token") String token);
 }
